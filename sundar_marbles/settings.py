febefa18ecbad0f92d184dba930de8effdd2412b
Hadeed711 <hadeedahmad711@gmail.com>
2025-08-02 19:51:31 +0500

    Fix CORS settings for localhost development

--- sundar_marbles/settings.py
+++ sundar_marbles/settings.py
@@ -195,7 +195,7 @@ REST_FRAMEWORK = {
 # CORS settings
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS', 
-    default='http://localhost:3000,http://127.0.0.1:3000',
+    default='http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173,http://127.0.0.1:5173',
     cast=lambda v: [s.strip() for s in v.split(',')]
 )
 
